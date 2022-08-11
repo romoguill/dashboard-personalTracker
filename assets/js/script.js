92525efd6cfac0d79346d9fbea653eca82ef2a66
romoguill <romoguill@gmail.com>
2022-08-11 11:41:19 -0300

    Added functionality to toggle the popup ctx menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,12 @@ const navToggleBtn = document.querySelector('[data-menu-toggle-btn]');
 navToggleBtn.addEventListener('click', () => {
   header.classList.toggle('active');
 });
+
+// Toggle ctx-menu when click on card-menu-btn
+const menuBtn = document.querySelectorAll('[data-menu-btn]');
+
+for (let i = 0; i < menuBtn.length; i++) {
+  menuBtn[i].addEventListener('click', function () {
+    this.nextElementSibling.classList.toggle('active');
+  });
+}
